ca1d7afba22675c43df519cc64d1fd533f2b1051
Cassandra Torske <cassandra.torske@gmail.com>
2017-07-20 13:46:41 -0600

    edit form hooked up

--- src/components/book/EditBookPage.js
+++ src/components/book/EditBookPage.js
@@ -2,12 +2,20 @@ import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import BookDetails from './BookDetails'
-import EditForm from './EditForm'
 import * as bookActions from '../../actions/bookActions'
 
 class EditBookPage extends React.Component {
   constructor(props, context) {
     super(props, context)
+    this.state = {
+      value: this.props.book.title
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
   }
 
   componentWillMount () {
@@ -22,7 +30,28 @@ class EditBookPage extends React.Component {
     return (
       <div>
         <h1>Book Edit Page</h1>
-        <EditForm book={this.props.book.title}/>
+        <section>
+        <form onSubmit={e => {
+          e.preventDefault()
+          props.submitEditBook(input)
+          e.target.reset()
+        }}
+        className="form-horizontal">
+        <div className="input-group">
+          <label className="col-sm-2 control-label">Title: </label>
+          <div className="col-sm-10">
+             <input type="text" value={this.state.value} onChange={this.handleChange} name="title"
+              className="form-control"/>
+          </div>
+        </div>
+        <br/>
+        <div className="input-group">
+          <div className="col-sm-offset-2 col-sm-10">
+            <input type="submit" className="btn btn-default"/>
+          </div>
+        </div>
+      </form>
+        </section>
       </div>
     )
   }
